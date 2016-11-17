5ed79ade833c62fd2572f8bbfdc338cf1676f02e
Paavo Pärssinen <paavo.parssinen@aalto.fi>
2016-11-17 18:44:17 +0200

    Added header-file for Hero-class

--- src/hero.cpp
+++ src/hero.cpp
@@ -1,33 +1,5 @@
+#include "hero.hpp"
 
-
-class Hero {
-	public:
-		Hero(int h, int s, int a, int d, int m);
-		void gainExp(int e);
-		void lvlUp(void);
-		bool takeDamage(int dmg);
-		void heal(int h);
-		int getHp();
-		int getStr();
-		int getLvl();
-		int getExp();
-		int getExpToNext();
-		int getAgi();
-		int getMana();
-		int getDef();
-		
-	private:
-		int maxhp;			//Hero's max health points	
-		int hp;				//Hero's current health points
-		int lvl;			//Level of the hero
-		int exp;			//Experience points
-		int strength;		//Hero's strength stat
-		int agility;		//Hero's agility stat
-		int defense;		//Hero's defense stat
-		int mana;			//Hero's mana stat
-		int expToNextLvl;	//Needed exp to next lvl
-		
-};
 		
 //Constructor		
 Hero::Hero(int h, int s, int a, int d, int m){
